ef2c72d6f6b745c068ab70ac29c0031e121c183a
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-08-23 22:24:41 +0530

    the player rolls die to get a number

--- src/com/snake/snakeandladder.java
+++ src/com/snake/snakeandladder.java
@@ -1,10 +1,18 @@
 package com.snake;
 
+import java.util.Random;
+
 public class snakeandladder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to the game of Snake and Ladder");
-	}
-
+	
+	int pos = 0;
+	System.out.println("player is at postion "+pos);
+	
+	Random random = new Random();
+	int dieNumber = random.nextInt(7-1)+1;
+	System.out.println("the die gives "+dieNumber);
+}
 }
