2ba28c28aa21197441068fe28035e32b99cc212d
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-08-23 22:10:56 +0530

    welcome message added

---
+++ src/com/snake/snakeandladder.java
@@ -0,0 +1,10 @@
+package com.snake;
+
+public class snakeandladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to the game of Snake and Ladder");
+	}
+
+}
