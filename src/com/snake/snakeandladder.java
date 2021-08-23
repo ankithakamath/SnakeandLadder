d9a4689cbdf283308246066b7676255dcacbe8be
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-08-23 22:46:24 +0530

    functionality for no play,ladder or snake added

--- src/com/snake/snakeandladder.java
+++ src/com/snake/snakeandladder.java
@@ -4,15 +4,31 @@ import java.util.Random;
 
 public class snakeandladder {
 
+	public static final int NO_PLAY = 1;
+	public static final int LADDER = 2;
+	public static final int SNAKE =3;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("welcome to the game of Snake and Ladder");
-	
-	int pos = 0;
-	System.out.println("player is at postion "+pos);
-	
-	Random random = new Random();
-	int dieNumber = random.nextInt(7-1)+1;
-	System.out.println("the die gives "+dieNumber);
+		System.out.println("Welcome to Snake and Ladder Problem");
+		
+
+		int pos= 0;
+		System.out.println("player is at position "+pos);
+		
+		Random random = new Random();
+		int dieNumber = random.nextInt(7-1)+1;
+		System.out.println("Die number : "+dieNumber);
+		
+		int choice = random.nextInt(4-1)+1;
+		
+		if(choice == NO_PLAY){
+			System.out.println("No Play");
+		}else if(choice == LADDER) {
+			System.out.println("Player climbed by "+dieNumber);
+			pos+=dieNumber;
+		}else if(choice == SNAKE) {
+			System.out.println("Player dropped by "+dieNumber);
+			pos-=dieNumber;
+		}
 }
 }
