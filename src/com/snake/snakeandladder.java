59afc7defc11f0040812aeeae42ed352e88ec466
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-08-23 22:13:48 +0530

    game begins at position zero

--- src/com/snake/snakeandladder.java
+++ src/com/snake/snakeandladder.java
@@ -5,6 +5,10 @@ public class snakeandladder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to the game of Snake and Ladder");
+	int pos=0;
+	System.out.println("player is at postion "+pos);
+	
+
 	}
 
 }
